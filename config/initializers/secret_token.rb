1e0253a17bbfc9e149775877a936a87cf466c57c
PITB <pitb@ubuntu.ubuntu-domain>
2013-06-17 16:46:00 +0300

    initial_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'fbdb8aedac21663f45c1d36a1bc806e4dfbfffa4b68654e792fde759046021b296bf2d766c2cbb15fe80ff10365589a24adb93f42be2ee6a39831d497ce5a505'
